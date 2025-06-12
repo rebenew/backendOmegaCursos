6a5c1ac1980942c61a504111c1c18c9b4686125c
rebenew <rebenews@gmail.com>
2025-06-11 19:46:12 -0500

    "Agregar funcionalidad de etiquetas: entidad, relación muchos-a-muchos con cursos y DTO Se creó la entidad Tag y se estableció la relación muchos-a-muchos con Course a través de la tabla intermedia 'course_tags'. - Se agregó CourseDTO para manejar la creación y obtención de cursos junto con sus etiquetas (List<String>)

---
+++ backend/src/main/java/com/cursos/backend/repository/TagRepository.java
@@ -0,0 +1,10 @@
+package com.cursos.backend.repository;
+
+import com.cursos.backend.model.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByName(String name);
+}
