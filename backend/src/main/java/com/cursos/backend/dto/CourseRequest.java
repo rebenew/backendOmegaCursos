2243de22a47bff3e6d1bd1c317e15cdedd44525a
ViviSuarez22 <vivisuarez22@hotmil.com>
2025-06-19 16:51:41 -0500

    Resquest

--- backend/src/main/java/com/cursos/backend/dto/CourseRequest.java
+++ backend/src/main/java/com/cursos/backend/dto/CourseRequest.java
@@ -1,4 +1,21 @@
 package com.cursos.backend.dto;
 
+import java.math.BigDecimal;
+import java.util.Set;
+
 public class CourseRequest {
+    private String title;
+    private String modality;
+    private String certification;
+    private String duration;
+    private String description;
+    private BigDecimal price;
+    private Set<TagDTO> tags;
+
+    // Getters y Setters
+
+    public static class TagDTO {
+        private String name;
+        // Getters y Setters
+    }
 }
