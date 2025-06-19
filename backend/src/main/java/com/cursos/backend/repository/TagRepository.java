a23dee225ae4c460456e31eec3b4430b1690cc4b
ViviSuarez22 <vivisuarez22@hotmil.com>
2025-06-19 16:54:22 -0500

    repositorio del tag

---
+++ backend/src/main/java/com/cursos/backend/repository/TagRepository.java
@@ -0,0 +1,12 @@
+package com.cursos.backend.repository;
+
+import com.cursos.backend.model.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByName(String name);
+} 
